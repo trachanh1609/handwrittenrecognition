d8962884d204c3817073bd72c0d88905a12f3827
Vu Nguyen <trachanh@gmail.com>
2018-09-19 14:57:51 +0300

    print python version to index

--- handwrittenregconition/views.py
+++ handwrittenregconition/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+import platform
 
 def index(request):
-    return HttpResponse("Hello world ! You're at the index page.")
+    return HttpResponse("Hello world ! Python version is " + platform.python_version() + ".")
