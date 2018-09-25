6a82d5555594c54d92d1218f87245d68c6d6304e
Vu Nguyen <trachanh@gmail.com>
2018-09-25 12:25:48 +0300

    change Django Static URL to client/build/static

--- devdemoapp/settings.py
+++ devdemoapp/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'client/build/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'client/build/static'),
